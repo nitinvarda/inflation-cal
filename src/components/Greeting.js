26704829bd0b1960de42b2ff207f712a71ca31c8
Nitin Varda <nitinvardha09@gmail.com>
2024-10-22 14:05:14 -0500

    Initial commit

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <View>
+      <Text>Greeting</Text>
+    </View>
+  )
+}
+
+export default Greeting
